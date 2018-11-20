22b697de7a21974ee6712afb4dcbf7d627ab4043
Jiahe Lyu <jiahe.lyu@mail.utoronto.ca>
2018-11-19 23:56:03 -0500

    Add the java activity file "Game2048Activity": Serve as the activity inflate of Game 2048 to connect the intent activivty_2048.xml.

---
+++ Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/Game2048Activity.java
@@ -0,0 +1,277 @@
+package group_0617.csc207.gamecentre;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Timer;
+import java.util.TimerTask;
+import pub.devrel.easypermissions.EasyPermissions;
+
+/**
+ * The game activity
+ */
+public class Game2048Activity extends AppCompatActivity implements Observer {
+
+    /**
+     * The board manager.
+     */
+    private BoardManager2048 boardManager2048;
+
+    /**
+     * The buttons to display.
+     */
+    private ArrayList<Button> tileButtons;
+
+    /**
+     * Constants for swiping directions. Should be an enum, probably.
+     */
+    public static final int UP = 101;
+    public static final int DOWN = 102;
+    public static final int LEFT = 103;
+    public static final int RIGHT = 104;
+
+    // Grid View and calculated column height and width based on device size
+    private GestureDetectGridView gridView;
+    private static int columnWidth, columnHeight;
+
+    /**
+     * The timer for the game.
+     */
+    private Timer timer = new Timer("GameActivityTimer");
+
+    /**
+     * The timer counts for the timer.
+     */
+    public int counts = 0;
+
+    /**
+     * The timer task for the timer.
+     */
+    private TimerTask timerTask = null;
+
+
+    /**
+     * Start the game timer at startValue.
+     *
+     * @param startValue the start value to set the counts of timer
+     */
+    private void startTimer(int startValue) {
+        counts = startValue;
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        counts++;
+                        TextView score = (TextView) findViewById(R.id.Score);
+                        score.setText("Time: "+counts);
+                        boardManager2048.setLastTime(counts);
+                        saveToFile("save_file_" +
+                                Board.NUM_COLS + "_" + LoginActivity.currentUser);
+                    }
+                });
+            }
+        };
+        timer.scheduleAtFixedRate(timerTask, new Date(), 1000);
+    }
+
+    /**
+     * Stop the timer and return counts (the stop time).
+     *
+     * @return the stop time counts.
+     */
+    public int stopTimer() {
+        timerTask.cancel();
+        timerTask = null;
+        return counts;
+    }
+
+    /**
+     * Set up the background image for each button based on the master list
+     * of positions, and then call the adapter to set the view.
+     */
+    // Display
+    public void display() {
+        updateTileButtons();
+        gridView.setAdapter(new CustomAdapter(tileButtons, columnWidth, columnHeight));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        loadFromFile(StartingActivity.TEMP_SAVE_FILENAME);
+        createTileButtons(this);
+        setContentView(R.layout.activity_2048);
+        addUndoButtonListener();
+
+        gridView = findViewById(R.id.grid);
+        gridView.setNumColumns(Board.NUM_COLS);
+        gridView.setBoardManager2048(boardManager2048);
+        boardManager2048.getBoard2048().addObserver(this);
+        // Observer sets up desired dimensions as well as calls our display function
+        gridView.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        gridView.getViewTreeObserver().removeOnGlobalLayoutListener(
+                                this);
+                        int displayWidth = gridView.getMeasuredWidth();
+                        int displayHeight = gridView.getMeasuredHeight();
+
+                        columnWidth = displayWidth / Board.NUM_COLS;
+                        columnHeight = displayHeight / Board.NUM_ROWS;
+
+                        display();
+                    }
+                });
+    }
+
+    /**
+     * Dispatch onPause() to fragments.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveToFile(StartingActivity.TEMP_SAVE_FILENAME);
+        boardManager2048.setLastTime(stopTimer());
+    }
+
+    /**
+     * Dispatch onResumee() to fragments.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startTimer(boardManager2048.getLastTime());
+    }
+
+    /**
+     * Create the buttons for displaying the tiles.
+     *
+     * @param context the context
+     */
+    private void createTileButtons(Context context) {
+        Board2048 board2048 = boardManager2048.getBoard2048();
+        tileButtons = new ArrayList<>();
+        for (int row = 0; row != Board.NUM_ROWS; row++) {
+            for (int col = 0; col != Board.NUM_COLS; col++) {
+                Button tmp = new Button(context);
+                tmp.setBackgroundResource(board2048.getTile(row, col).getBackground());
+                this.tileButtons.add(tmp);
+            }
+        }
+    }
+
+    /**
+     * Update the backgrounds on the buttons to match the tiles.
+     */
+    private void updateTileButtons() {
+        //System.out.println("1: " + boardManager2048.getHa());
+        Board2048 board2048 = boardManager2048.getBoard2048();
+        int nextPos = 0;
+        for (Button b : tileButtons) {
+            int row = nextPos / Board.NUM_ROWS;
+            int col = nextPos % Board.NUM_COLS;
+            b.setBackgroundResource(board2048.getTile(row, col).getBackground());
+            nextPos++;
+        }
+    }
+
+    /**
+     * Load the board manager from fileName.
+     *
+     * @param fileName the name of the file
+     */
+    private void loadFromFile(String fileName) {
+        try {
+            InputStream inputStream = this.openFileInput(fileName);
+            if (inputStream != null) {
+                ObjectInputStream input = new ObjectInputStream(inputStream);
+                boardManager2048 = (BoardManager2048) input.readObject();
+                inputStream.close();
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        } catch (ClassNotFoundException e) {
+            Log.e("login activity", "File contained unexpected data type: " + e.toString());
+        }
+    }
+
+    /**
+     * Save the board manager to fileName.
+     *
+     * @param fileName the name of the file
+     */
+    public void saveToFile(String fileName) {
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(
+                    this.openFileOutput(fileName, MODE_PRIVATE));
+            outputStream.writeObject(boardManager2048);
+            outputStream.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    /**
+     * Activate the Undo button.
+     */
+    private void addUndoButtonListener() {
+        Button undoButton = findViewById(R.id.btnundo);
+        undoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean undoAvailable = boardManager2048.undo();
+                if (undoAvailable) {
+                    displayToast("Undo successfully! You have made " + boardManager2048.getTimesOfUndo() + " times of undo.");
+                } else {
+                    displayToast("Undo failed! This is the original board.");
+                }
+            }
+        });
+    }
+
+    /**
+     * Display that a toast of undo.
+     */
+    private void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        display();
+    }
+
+
+}
