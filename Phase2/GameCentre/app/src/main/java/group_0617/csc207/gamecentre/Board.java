fda1e2e45c986155059057b2095dfccc154a3e19
Jiahe Lyu <jiahe.lyu@mail.utoronto.ca>
2018-11-20 21:50:05 -0500

    Create a superclass Board for Board2048 and BoardSlidingtiles. Combine their common variables and methods, especially the static variables NUM_COLS and NUM_ROWS.

--- Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/Board.java
+++ Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/Board.java
@@ -13,7 +13,7 @@ import java.util.List;
  * The sliding tiles board.
  * Which implement Iterable<Tile> and Serializable.
  */
-public class Board extends Observable implements Serializable, Iterable<Tile> {
+public class Board extends Observable implements Serializable{
 
     /**
      * The number of rows.
@@ -30,6 +30,14 @@ public class Board extends Observable implements Serializable, Iterable<Tile> {
      */
     private Tile[][] tiles = new Tile[NUM_ROWS][NUM_COLS];
 
+    Board(){
+//        for (int row = 0; row != Board.NUM_ROWS; row++) {
+//            for (int col = 0; col != Board.NUM_COLS; col++) {
+//                this.tiles[row][col] = new Tile(0);
+//            }
+//        }
+    }
+
     /**
      * A new board of tiles in row-major order.
      * Precondition: len(tiles) == NUM_ROWS * NUM_COLS
@@ -38,7 +46,6 @@ public class Board extends Observable implements Serializable, Iterable<Tile> {
      */
     Board(List<Tile> tiles) {
         Iterator<Tile> iter = tiles.iterator();
-
         for (int row = 0; row != Board.NUM_ROWS; row++) {
             for (int col = 0; col != Board.NUM_COLS; col++) {
                 this.tiles[row][col] = iter.next();
@@ -66,100 +73,6 @@ public class Board extends Observable implements Serializable, Iterable<Tile> {
         return tiles[row][col];
     }
 
-    /**
-     * Swap the tiles at (row1, col1) and (row2, col2)
-     *
-     * @param row1 the first tile row
-     * @param col1 the first tile col
-     * @param row2 the second tile row
-     * @param col2 the second tile col
-     */
-    void swapTiles(int row1, int col1, int row2, int col2) {
-        Tile original_tile = getTile(row1, col1);
-        tiles[row1][col1] = getTile(row2, col2);
-        tiles[row2][col2] = original_tile;
-        setChanged();
-        notifyObservers();
-    }
-
-    /**
-     * Count the number of inversion for current tile specified by row and column number
-     * @param row the row of current tile
-     * @param col the column of current tile
-     * @return the number of inversion for current tile
-     */
-    private int countInversion(int row, int col){
-        int inv = 0;
-        int pos = row * NUM_COLS + col;
-        int posToCompare = pos+1;
-        Tile currentTile = getTile(row, col);
-        Tile tileToCompare;
-        while (posToCompare <= numTiles()) {
-            tileToCompare = getTile(posToCompare/NUM_COLS, posToCompare%NUM_COLS);
-            if (currentTile.getId() > tileToCompare.getId()) {
-                inv++;
-            }
-            posToCompare++;
-        }
-
-        return inv;
-    }
-
-    /**
-     * Return the sum of polarity over all tiles in the blank.
-     *
-     * @return the sum of polarity over all tiles in the blank.
-     */
-    private int sumOverPolarity() {
-
-        int totInv = 0;
-        for (int i = 0; i < NUM_COLS; i++) {
-            for (int j = 0; j < NUM_ROWS; j++) {
-                totInv = totInv + countInversion(i, j);
-            }
-        }
-        return totInv;
-    }
-
-    /**
-     * Check if the board is solvable
-     * @return if the board is solvable
-     */
-    private boolean isSolvable() {
-        boolean isEvenPol = sumOverPolarity()/2 == 0;
-        return NUM_COLS/2 == 1 && isEvenPol || NUM_COLS/2 == 0 && blankOnOddRowFromBottom() == isEvenPol;
-    }
-
-    /**
-     * This method check if the board is solvable. If not, it makes the board solvable
-     */
-    public void makeSolvable() {
-        if (!isSolvable()) {
-            if (getTile(0, 0).getId() == numTiles() || getTile(1, 0).getId() == numTiles()) {
-                swapTiles(NUM_ROWS-1, NUM_COLS-1, NUM_ROWS-1, NUM_COLS-2);
-            } else {
-                swapTiles(0, 0, 1, 0);
-            }
-        }
-    }
-
-    /**
-     * Check if blank tile is on odd row counting from bottom
-     * @return if blank tile is on odd row counting from bottom
-     */
-    private boolean blankOnOddRowFromBottom() {
-        boolean re = false;
-        for (int row = this.NUM_ROWS; row >= 0; row++) {
-            if ((this.NUM_ROWS - row) / 2 == 1) {
-                for (int col = 0; col < this.NUM_COLS; col++) {
-                    if (getTile(row, col).getId() == numTiles()) {
-                        re = true;
-                    }
-                }
-            }
-        }
-        return re;
-    }
 
     @Override
     public String toString() {
@@ -168,40 +81,40 @@ public class Board extends Observable implements Serializable, Iterable<Tile> {
                 '}';
     }
 
-    @NonNull
-    @Override
-    public Iterator<Tile> iterator() {
-        return new BoardIterator();
-    }
-
-    private class BoardIterator implements Iterator<Tile> {
-        int rowIndex = 0;
-        int columnIndex = 0;
-
-        @Override
-        public boolean hasNext() {
-            return rowIndex != NUM_ROWS - 1 ||
-                    columnIndex != NUM_COLS - 1;
-        }
-
-        @Override
-        public Tile next() {
-            Tile result = tiles[rowIndex][columnIndex];
-            if (rowIndex != Board.NUM_ROWS - 1) {
-                if (columnIndex != Board.NUM_COLS - 1) {
-                    columnIndex++;
-                } else {
-                    rowIndex++;
-                    columnIndex = 0;
-                }
-            } else {
-                if (columnIndex != Board.NUM_COLS - 1) {
-                    columnIndex++;
-                }
-            }
-            return result;
-        }
-
-    }
+//    @NonNull
+//    @Override
+//    public Iterator<Tile> iterator() {
+//        return new BoardIterator();
+//    }
+//
+//    private class BoardIterator implements Iterator<Tile> {
+//        int rowIndex = 0;
+//        int columnIndex = 0;
+//
+//        @Override
+//        public boolean hasNext() {
+//            return rowIndex != NUM_ROWS - 1 ||
+//                    columnIndex != NUM_COLS - 1;
+//        }
+//
+//        @Override
+//        public Tile next() {
+//            Tile result = tiles[rowIndex][columnIndex];
+//            if (rowIndex != Board.NUM_ROWS - 1) {
+//                if (columnIndex != Board.NUM_COLS - 1) {
+//                    columnIndex++;
+//                } else {
+//                    rowIndex++;
+//                    columnIndex = 0;
+//                }
+//            } else {
+//                if (columnIndex != Board.NUM_COLS - 1) {
+//                    columnIndex++;
+//                }
+//            }
+//            return result;
+//        }
+//
+//    }
 
 }
