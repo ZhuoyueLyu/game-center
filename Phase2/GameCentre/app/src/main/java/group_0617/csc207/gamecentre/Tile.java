a6fe753b2498e8626f51c68ddf0f26e99eb57c4b
thomsonyen <thomsonyen19981114@gmail.com>
2018-11-24 14:16:52 -0500

    Make Tile extend from GenericTile. Sitting with Jiahe, Zhouyue.

--- Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/Tile.java
+++ Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/Tile.java
@@ -11,12 +11,7 @@ import group_0617.csc207.gamecentre.R;
 /**
  * A Tile in a sliding tiles puzzle.
  */
-public class Tile implements Comparable<Tile>, Serializable {
-
-    /**
-     * The background id to find the tile image.
-     */
-    private int background;
+public class Tile  extends GenericTile implements Comparable<Tile> {
 
     /**
      * The unique id.
@@ -38,24 +33,100 @@ public class Tile implements Comparable<Tile>, Serializable {
      * A tile with a background id; look up and set the id.
      *
      * @param backgroundId
+     * @param currentBoardComplexity the complexity of current board
      */
-    public Tile(int backgroundId, int complexity) {
+    public Tile(int backgroundId, int currentBoardComplexity) {
         id = backgroundId;
+        switch (backgroundId + 1) {
+            case 1:
+                setBackground(R.drawable.tile_1);
+                break;
+            case 2:
+                setBackground(R.drawable.tile_2);
+                break;
+            case 3:
+                setBackground(R.drawable.tile_3);
+                break;
+            case 4:
+                setBackground(R.drawable.tile_4);
+                break;
+            case 5:
+                setBackground(R.drawable.tile_5);
+                break;
+            case 6:
+                setBackground(R.drawable.tile_6);
+                break;
+            case 7:
+                setBackground(R.drawable.tile_7);
+                break;
+            case 8:
+                setBackground(R.drawable.tile_8);
+                break;
+            case 9:
+                if (currentBoardComplexity == 3){
+                    setBackground(R.drawable.tile_0);
+                }
+                else {
+                    setBackground(R.drawable.tile_9);
+                }
+                break;
+            case 10:
+                setBackground(R.drawable.tile_10);
+                break;
+            case 11:
+                setBackground(R.drawable.tile_11);
+                break;
+            case 12:
+                setBackground(R.drawable.tile_12);
+                break;
+            case 13:
+                setBackground(R.drawable.tile_13);
+                break;
+            case 14:
+                setBackground(R.drawable.tile_14);
+                break;
+            case 15:
+                setBackground(R.drawable.tile_15);
+                break;
+            case 16:
+                if (currentBoardComplexity == 4){
+                    setBackground(R.drawable.tile_0);
+                }
+                else {
+                    setBackground(R.drawable.tile_16);
+                }
+                break;
+            case 17:
+                setBackground(R.drawable.tile_17);
+                break;
+            case 18:
+                setBackground(R.drawable.tile_18);
+                break;
+            case 19:
+                setBackground(R.drawable.tile_19);
+                break;
+            case 20:
+                setBackground(R.drawable.tile_20);
+                break;
+            case 21:
+                setBackground(R.drawable.tile_21);
+                break;
+            case 22:
+                setBackground(R.drawable.tile_22);
+                break;
+            case 23:
+                setBackground(R.drawable.tile_23);
+                break;
+            case 24:
+                setBackground(R.drawable.tile_24);
+                break;
+            case 25:
+                setBackground(R.drawable.tile_0);
+                break;
+            default:
+                setBackground(R.drawable.tile_0);
+        }
     }
-
-    /**
-     * Return the background id.
-     *
-     * @return the background id
-     */
-    public int getBackground() {
-        return background;
-    }
-
-    public void setBackground(int background) {
-        this.background = background;
-    }
-
 
     /**
      * Return the tile id.
