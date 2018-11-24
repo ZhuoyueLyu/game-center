0bb9856962cb7ff9c9c3f9f5ded6ca3eae016001
thomsonyen <thomsonyen19981114@gmail.com>
2018-11-24 14:06:51 -0500

    A Generic Tile to extend from implemented.

---
+++ Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/GenericTile.java
@@ -0,0 +1,40 @@
+package group_0617.csc207.gamecentre;
+import java.io.Serializable;
+
+/**
+ * A Generic Tile with background id
+ */
+public abstract class GenericTile implements Serializable{
+
+    /**
+     * The background
+     */
+    private int background;
+
+    /**
+     * Create an empty tile
+     */
+    public GenericTile() {}
+
+    /**
+     * Create a Tile with specified background
+     * @param inBackground the background of the tile
+     */
+    public GenericTile(int inBackground) {
+        this.background = inBackground;
+    }
+
+    /**
+     * Return the background of the tile
+     * @return the background of the tile
+     */
+    public int getBackground() {
+        return background;
+    }
+
+    /**
+     * Set the background of the tiles
+     * @param inBackground the new background of the tile
+     */
+    public void setBackground(int inBackground) {this.background = inBackground; }
+}
