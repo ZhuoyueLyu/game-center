5b9d52e28aa5b997ceb61bd9847084954fc47846
thomsonyen <thomsonyen19981114@gmail.com>
2018-11-24 15:04:24 -0500

    Fix how Tile is initiated. Sitting with Jiahe, Zhouyue.

--- Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/Tile.java
+++ Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/Tile.java
@@ -36,7 +36,7 @@ public class Tile  extends GenericTile implements Comparable<Tile> {
      * @param currentBoardComplexity the complexity of current board
      */
     public Tile(int backgroundId, int currentBoardComplexity) {
-        id = backgroundId;
+        id = backgroundId + 1;
         switch (backgroundId + 1) {
             case 1:
                 setBackground(R.drawable.tile_1);
