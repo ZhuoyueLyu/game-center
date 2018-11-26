9ab9d2b4de690ad0802d9980a23161b95ab88d23
thomsonyen <thomsonyen19981114@gmail.com>
2018-11-26 13:33:54 -0500

    Unit test for Tile added.

---
+++ Phase2/GameCentre/app/src/test/java/group_0617/csc207/gamecentre/TileTest.java
@@ -0,0 +1,92 @@
+package group_0617.csc207.gamecentre;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for Tile
+ */
+public class TileTest {
+
+    /**
+     * The tile to test
+     */
+    private Tile tile;
+
+    /**
+     * Initiate the tile to test
+     */
+    private void setUpTile() {
+        tile = new Tile(1, 4);
+    }
+
+    /**
+     * Test whether the constructor works
+     */
+    @Test
+    public void testConstructor() {
+        List<Integer> backgrounds = new ArrayList<>();
+        backgrounds.add(R.drawable.tile_1);
+        backgrounds.add(R.drawable.tile_2);
+        backgrounds.add(R.drawable.tile_3);
+        backgrounds.add(R.drawable.tile_4);
+        backgrounds.add(R.drawable.tile_5);
+        backgrounds.add(R.drawable.tile_6);
+        backgrounds.add(R.drawable.tile_7);
+        backgrounds.add(R.drawable.tile_8);
+        backgrounds.add(R.drawable.tile_9);
+        backgrounds.add(R.drawable.tile_10);
+        backgrounds.add(R.drawable.tile_11);
+        backgrounds.add(R.drawable.tile_12);
+        backgrounds.add(R.drawable.tile_13);
+        backgrounds.add(R.drawable.tile_14);
+        backgrounds.add(R.drawable.tile_15);
+        backgrounds.add(R.drawable.tile_16);
+        backgrounds.add(R.drawable.tile_17);
+        backgrounds.add(R.drawable.tile_18);
+        backgrounds.add(R.drawable.tile_19);
+        backgrounds.add(R.drawable.tile_20);
+        backgrounds.add(R.drawable.tile_21);
+        backgrounds.add(R.drawable.tile_22);
+        backgrounds.add(R.drawable.tile_23);
+        backgrounds.add(R.drawable.tile_24);
+        for (int i = 0; i < 24; i++) {
+            tile = new Tile(i, 5);
+            assertEquals((long)backgrounds.get(i), tile.getBackground());
+        }
+        tile = new Tile(24, 5);
+        assertEquals(R.drawable.tile_0, tile.getBackground());
+        tile = new Tile(8, 3);
+        assertEquals(R.drawable.tile_0, tile.getBackground());
+        tile = new Tile(15, 4);
+        assertEquals(R.drawable.tile_0, tile.getBackground());
+
+        // Give something undefined
+        tile = new Tile(1000, 7);
+        assertEquals(R.drawable.tile_0, tile.getBackground());
+
+    }
+
+    /**
+     * Test whether getId works
+     */
+    @Test
+    public void testGetId() {
+        setUpTile();
+        assertEquals(2, tile.getId());
+    }
+
+    /**
+     * Test whether compareTo works
+     */
+    @Test
+    public void testCompareTo() {
+        setUpTile();
+        Tile tileToCompare = new Tile(3, 4);
+        assertEquals(2, tile.compareTo(tileToCompare));
+    }
+}
