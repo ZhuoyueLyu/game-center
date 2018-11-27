28cc46c2a1d419e3d27c08ed1e4bf02dad64d79d
Zhuoyue Lyu <zhuoyue.lyu@mail.utoronto.ca>
2018-11-27 15:16:17 -0500

    Add javadoc for Tuple class (explain the meaning of x and y)

--- Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/Tuple.java
+++ Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/Tuple.java
@@ -2,30 +2,58 @@ package group_0617.csc207.gamecentre;
 
 /**
  * This tuple class is used to transfer the game data to the leader board
+ *
  * @param <X> username
  * @param <Y> the score of the user in a specific game
  */
-public class Tuple<X, Y> {
-    private  X x;
-    private  Y y;
-    public Tuple(X x, Y y) {
+class Tuple<X,Y> {
+    private X x;
+    private Y y;
+
+    /**
+     * Constructor
+     *
+     * @param x username
+     * @param y score
+     */
+    Tuple(X x,Y y) {
         this.x = x;
         this.y = y;
     }
 
-    public X getX() {
+    /**
+     * Get the value of x (username)
+     *
+     * @return value of x (username)
+     */
+    X getX() {
         return x;
     }
 
-    public void setX(X x) {
+    /**
+     * Set the value of x (username)
+     *
+     * @param x value of x (username)
+     */
+    void setX(X x) {
         this.x = x;
     }
 
-    public Y getY() {
+    /**
+     * Get the value of y (score)
+     *
+     * @return value of y (score)
+     */
+    Y getY() {
         return y;
     }
 
-    public void setY(Y y) {
+    /**
+     * Set the value of y (score)
+     *
+     * @param y value of y (score)
+     */
+    void setY(Y y) {
         this.y = y;
     }
 }
