ec3acc7ee5f33ecc2334ffd80bd5f645bbd8d530
thomsonyen <thomsonyen19981114@gmail.com>
2018-11-27 22:21:08 -0500

    Change asserted value in Card's test to cope with new cover.

--- Phase2/GameCentre/app/src/test/java/group_0617/csc207/gamecentre/CardTest.java
+++ Phase2/GameCentre/app/src/test/java/group_0617/csc207/gamecentre/CardTest.java
@@ -72,7 +72,7 @@ public class CardTest {
 
         // executes default that set background to tile0
         card = new Card(1000);
-        assertEquals(card.getBackground(), R.drawable.tile_0);
+        assertEquals(card.getBackground(), R.drawable.question);
     }
 
 
@@ -111,7 +111,7 @@ public class CardTest {
     @Test
     public void testGetDisplay() {
         setUpCard();
-        assertEquals(R.drawable.tile_0, card.getDisplay());
+        assertEquals(R.drawable.question, card.getDisplay());
         card.flip();
         assertEquals(card.getBackground(), card.getDisplay());
     }
