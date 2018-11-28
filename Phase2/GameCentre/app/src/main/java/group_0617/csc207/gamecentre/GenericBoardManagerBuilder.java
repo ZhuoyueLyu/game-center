92039aca4a1833399d374f184b9e37b30bf775e1
thomsonyen <thomsonyen19981114@gmail.com>
2018-11-28 17:39:05 -0500

    Add Builder and saveLoader for GenericBoardManager for setup in activities.

---
+++ Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/GenericBoardManagerBuilder.java
@@ -0,0 +1,57 @@
+package group_0617.csc207.gamecentre;
+
+import group_0617.csc207.gamecentre.gameMemory.CardBoardManager;
+import group_0617.csc207.gamecentre.gameSlidingTiles.BoardManager;
+import group_0617.csc207.gamecentre.game2048.BoardManager2048;
+
+/**
+ * The builder that builds specified type of BoardManager
+ */
+public class GenericBoardManagerBuilder {
+
+    /**
+     * The only instance of GenericBoardManagerBuilder
+     */
+    private GenericBoardManagerBuilder theBuilder = null;
+
+    /**
+     * An empty constructor that does nothing
+     */
+    private GenericBoardManagerBuilder() {
+    }
+
+    /**
+     * Return the only instance of GenericBoardManagerBuilder
+     * @return the only instance of GenericBoardManagerBuilder
+     */
+    public GenericBoardManagerBuilder getInstance() {
+        if (theBuilder == null) {
+            theBuilder = new GenericBoardManagerBuilder();
+        }
+        return theBuilder;
+    }
+
+    /**
+     * Return the specified version of BoardManager given complexity.
+     * @param game the string that specified the BoardManager to return
+     * @param complexity the complexity of the GenericBoard of the returned BoardManager
+     * @return the GenericBoardManager of the type specified
+     */
+    public GenericBoardManager getGenericBoardManager(String game, int complexity) {
+        GenericBoardManager re;
+        switch (game) {
+            case "2048":
+                re = new BoardManager2048(complexity);
+                break;
+            case "st":
+                re = new BoardManager(complexity);
+                break;
+            case "memory":
+                re = new CardBoardManager(complexity);
+                break;
+            default:
+                re = null;
+        }
+        return re;
+    }
+}
---
+++ Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/GenericBoardManagerSaveLoader.java
@@ -0,0 +1,86 @@
+package group_0617.csc207.gamecentre;
+
+
+import android.util.Log;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import android.content.Context;
+
+/**
+ * The class responsible for saving BoardManager
+ */
+public class GenericBoardManagerSaveLoader {
+
+    /**
+     * The only instance of this class
+     */
+    private static GenericBoardManagerSaveLoader theSaveLoader = null;
+
+    /**
+     * An empty constructor
+     */
+    private GenericBoardManagerSaveLoader() {
+    }
+
+    /**
+     * Return the only instance of this class, if does not exist create a new one
+     * @return the only instance of this class
+     */
+    public static GenericBoardManagerSaveLoader getInstance() {
+
+        if (theSaveLoader == null) {
+            theSaveLoader = new GenericBoardManagerSaveLoader();
+        }
+        return theSaveLoader;
+    }
+
+    /**
+     * Save the given GenericBoardManager and save it in the specified path under given context
+     * @param inGenericBoardManager the GenericBoardManager to save
+     * @param inSavePath the path to save the GenericBoardManager to
+     * @param context the context
+     */
+    public void saveGenericBoardManager(GenericBoardManager inGenericBoardManager, String inSavePath,
+                                        Context context) {
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(
+                    context.openFileOutput(inSavePath, context.MODE_PRIVATE));
+            outputStream.writeObject(inGenericBoardManager);
+            outputStream.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    /**
+     * Return the GenericBoardManager stored in the given path under given context.
+     * Return null if errors occurred in reading or if no object is found.
+     * @param inLoadPath the path to load GenericBoardManager from
+     * @param context the context
+     * @return the GenericBoardManager stored. null if not found or error occurred
+     */
+    public GenericBoardManager loadGenericBoardManager(String inLoadPath, Context context) {
+        try {
+            InputStream inputStream = context.openFileInput(inLoadPath);
+            if (inputStream != null) {
+                ObjectInputStream input = new ObjectInputStream(inputStream);
+                GenericBoardManager myBoardManager = (GenericBoardManager) input.readObject();
+                inputStream.close();
+                return myBoardManager;
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        } catch (ClassNotFoundException e) {
+            Log.e("login activity", "File contained unexpected data type: " + e.toString());
+        }
+        return null;
+    }
+
+}
