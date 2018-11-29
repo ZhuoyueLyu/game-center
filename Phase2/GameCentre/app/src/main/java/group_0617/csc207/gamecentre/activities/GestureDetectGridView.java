3707806a475dccacdac7b6af1f74da1baa799f35
Jiahe Lyu <jiahe.lyu@mail.utoronto.ca>
2018-11-28 23:36:22 -0500

    Reformat the codes in GestureDetectGridView.java

--- Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/activities/GestureDetectGridView.java
+++ Phase2/GameCentre/app/src/main/java/group_0617/csc207/gamecentre/activities/GestureDetectGridView.java
@@ -1,12 +1,5 @@
 package group_0617.csc207.gamecentre.activities;
 
-/**
-Adapted from:
-https://github.com/DaveNOTDavid/sample-puzzle/blob/master/app/src/main/java/com/davenotdavid/samplepuzzle/GestureDetectGridView.java
-
-This extension of GridView contains built in logic for handling swipes between buttons
- */
-
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
@@ -18,9 +11,14 @@ import android.widget.GridView;
 import group_0617.csc207.gamecentre.GenericBoardManager;
 import group_0617.csc207.gamecentre.game2048.Game2048Activity;
 
+/**
+ * Adapted from:
+ * https://github.com/DaveNOTDavid/sample-puzzle/blob/master/app/src/main/java/com/davenotdavid/samplepuzzle/GestureDetectGridView.java
+ * <p>
+ * This extension of GridView contains built in logic for handling swipes between buttons
+ */
 public class GestureDetectGridView extends GridView {
     public static final int SWIPE_MIN_DISTANCE = 100;
-    public static final int SWIPE_MAX_OFF_PATH = 100;
     public static final int SWIPE_THRESHOLD_VELOCITY = 25;
 
     private GestureDetector gDetector;
@@ -66,10 +64,7 @@ public class GestureDetectGridView extends GridView {
             }
 
             @Override
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-                                   float velocityY) {
-                //final int position = GestureDetectGridView.this.pointToPosition
-                //(Math.round(e1.getX()), Math.round(e1.getY()));
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
                 if (ableToFling) {
                     int direction = 0;
 
@@ -78,11 +73,9 @@ public class GestureDetectGridView extends GridView {
                             return false;
                         }
                         if (e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE) {
-                            //MainActivity.moveTiles(context, MainActivity.up, position);
                             System.out.println("Up!");
                             direction = Game2048Activity.UP;
                         } else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE) {
-                            //MainActivity.moveTiles(context, MainActivity.down, position);
                             System.out.println("Down!");
                             direction = Game2048Activity.DOWN;
                         }
@@ -91,11 +84,9 @@ public class GestureDetectGridView extends GridView {
                             return false;
                         }
                         if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE) {
-                            //MainActivity.moveTiles(context, MainActivity.left, position);
                             System.out.println("Left!");
                             direction = Game2048Activity.LEFT;
                         } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE) {
-                            //MainActivity.moveTiles(context, MainActivity.right, position);
                             System.out.println("Right!");
                             direction = Game2048Activity.RIGHT;
                         }
